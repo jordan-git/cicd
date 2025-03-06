1e5ba8bcabf0f8f64bf3bfc198e49d68c5719092
Jordan Quinlan <jordan@jordan-pc.lan>
2025-03-06 18:26:38 +0000

    Fixed path for SSL files

--- fetch-ssl.sh
+++ fetch-ssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SSL_CERT_PATH="~/app/server.crt"
-SSL_KEY_PATH="~/app/privatekey.pem"
+SSL_CERT_PATH="/home/node/app/server.crt"
+SSL_KEY_PATH="/home/node/app/privatekey.pem"
 
 echo "📥 Fetching SSL certificates from S3..."
 aws s3 cp "s3://$S3_BUCKET_NAME/server.crt" "$SSL_CERT_PATH"
