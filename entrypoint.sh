4dd69312db2837d60fc46c6ebab87d211f9efde0
Jordan Quinlan <jordan@jordan-pc.lan>
2025-03-06 18:08:51 +0000

    Re-added PM2 and added absolute path for .sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running fetch-ssl.sh to get SSL files..."
-~/app/fetch-ssl.sh
+/home/node/app/fetch-ssl.sh
 
-echo "Starting app"
-exec node ~/app/bin/www
+echo "Starting app with PM2..."
+exec pm2-runtime bin/www
