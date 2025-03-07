8cd13c42d6b3bcdeff5cf66a2864b925a4233333
Jordan Quinlan <jordan@localhost.localdomain>
2025-03-07 09:41:09 +0000

    Added comments to explain CICD where necessary [skip ci]

--- fetch-ssl.sh
+++ fetch-ssl.sh
@@ -3,11 +3,12 @@
 SSL_CERT_PATH="/home/node/app/server.crt"
 SSL_KEY_PATH="/home/node/app/privatekey.pem"
 
-echo "📥 Fetching SSL certificates from S3..."
+echo "Fetching SSL certificates from S3..."
 aws s3 cp "s3://$S3_BUCKET_NAME/server.crt" "$SSL_CERT_PATH"
 aws s3 cp "s3://$S3_BUCKET_NAME/privatekey.pem" "$SSL_KEY_PATH"
 
-chmod 600 "$SSL_KEY_PATH"
-chmod 644 "$SSL_CERT_PATH"
+# Make sure the files are only readable by the owner
+chmod 400 "$SSL_KEY_PATH"
+chmod 400 "$SSL_CERT_PATH"
 
 echo "SSL certificates downloaded successfully."
